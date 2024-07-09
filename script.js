686c40cddb98b3b078aecaad56a8b8c85d3fcdf4
Somraj-234 <jadhavsomraj234@gmail.com>
2024-07-09 17:40:42 +0530

    page 1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function loadingAnimation() {
+    var tl = gsap.timeline()
 //bottom to up animation
 tl.from(".line h1",{
     y:150,
@@ -27,8 +28,8 @@ tl.from("#line1-part1, .line h2", {
 //hide loader
 tl.to("#loader",{
     opacity:0,
-    duration:0.2,
-    delay:4
+    duration:0, //0.2
+    delay:0 //4
 });
 tl.from("#page1",{
     y:1600,
@@ -39,3 +40,29 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    stagger:0.2,
+    y:120
+})
+
+}
+
+function cursorAnimation(){
+    //cursor move
+ document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+ })
+
+
+ //magnet effect
+ Shery.makeMagnet("#nav-part2 h4",{});
+}
+
+loadingAnimation();
+cursorAnimation();
